012abbb8834f560f5336ced47d874e59193cfd03
Dan Petersen <dp@danitso.com>
2019-05-11 19:38:40 +0200

    Fixed dependency issue in example.

--- example/data_source_queues.tf
+++ example/data_source_queues.tf
@@ -1,5 +1,10 @@
 data "ironio_queues" "example" {
-  depends_on  = ["ironio_project.example"]
+  depends_on = [
+    "ironio_pull_queue.example",
+    "ironio_pull_queue.example_push_error",
+    "ironio_push_queue.example",
+  ]
+
   filter_name = "${ironio_project.example.name}*"
   project_id  = "${ironio_project.example.id}"
 }
